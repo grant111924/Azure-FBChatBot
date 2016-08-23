9598e92d12d48068291c540a6a5d9fe1aa89a7e1
Grant <grant111924@gmail.com>
2016-08-23 14:03:53 +0800

    加入express模組跟app.j測試Code

---
+++ app.js
@@ -0,0 +1,14 @@
+var express=requre('express');
+var app=express();
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'grantbot') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+module.exports = app;
