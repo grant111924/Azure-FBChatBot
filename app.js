5e44a3639934b9b67dc528d7a8fdba5d15b95dc5
Grant <grant111924@gmail.com>
2016-08-23 16:59:01 +0800

    使用messenger-bot模組範例

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var express=require('express');
-var app=express();
-app.get('/',function(req, res){
-  res.send("good");
+var http = require('http')
+var Bot = require('messenger-bot')
+
+var bot = new Bot({
+  token: 'EAAHnpYruwVQBAI5wZCdL8wEGegQ50R3tkbcdafWck6r206pA8xVRyPjmKG7tIJZCGD7ifwZCoO8qITluWGQ5DJ7diAq5UZB7tCANommOIC6brp93b7ljTZCberBtY0Bq3KrCzrxQ5brZASY65KMGyQ97OeNmmqeiaELaY1gkK39AZDZD',
+  verify: 'grantbot',
+  app_secret: '2673dfa5a630efb487245039b42f1083'
 })
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'grantbot') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
- 
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
 
+bot.on('error', (err) => {
+  console.log(err.message)
+})
 
-app.listen(process.env.PORT);
+bot.on('message', (payload, reply) => {
+  var text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+http.createServer(bot.middleware()).listen(3000)
+console.log('Echo bot server running at port 3000.')
