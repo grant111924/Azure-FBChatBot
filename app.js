36dfaaae2cf5b7e4c15fc9532ff1845cf7c060e3
Grant <grant111924@gmail.com>
2016-08-23 15:04:21 +0800

    加入根目錄

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express=require('express');
 var app=express();
-
+app.get('/',function(req, res){
+  res.send("good");
+})
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === 'grantbot') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
-    res.send("good");
+ 
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);          
   }  
 });
+
 
 app.listen(process.env.PORT);
