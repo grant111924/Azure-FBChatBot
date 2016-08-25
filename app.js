e5384f6f46166ec7e825d282659406bfbe6d7243
Grant <grant111924@gmail.com>
2016-08-25 14:43:20 +0800

    測試button2

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.listen(app.get('port'), function() {
 //     res.sendStatus(200)
 // })
 app.post('/webhook/', function (req, res) {
+     getstartbutton();
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
