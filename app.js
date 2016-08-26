b47c823f0ab53a703906fd98b069f798c695c05d
Grant <grant111924@gmail.com>
2016-08-26 17:32:37 +0800

    起程站置到站

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.listen(app.get('port'), function() {
 // })
 app.post('/webhook/', function (req, res) {
     //  welcome();
-     getstartbutton();
+    //  getstartbutton();
   
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
@@ -128,33 +128,72 @@ function getstartbutton(){
 
 
 
-
+var station='[{"站名": "台中","時刻表": [{"車次": " 502","時間": "06:25"}, {"車次": "1504","時間": "06:45"}, {"車次": "1508","時間": "07:05"}, {"車次": "1510","時間": "07:17"}, {"車次": "802","時間": "07:25"}, {"車次": "1202","時間": "07:21"}, {"車次": "1602","時間": "07:40"}, {"車次": "1302","時間": "07:53"}, {"車次": "204","時間": "07:48"}, {"車次": "606","時間": "08:00"}, {"車次": "1514","時間": "08:04"}]},{"站名": "苗栗","時刻表": [{"車次": " 502","時間": "06:44"}, {"車次": "1504","時間": "07:05"}, {"車次": "1508","時間": "07:25"}, {"車次": "1510","時間": "07:37"}, {"車次": "802","時間": "07:45"}, {"車次": "1514","時間": "08:24"}]}]';
+var trainGo='[{"車次": "502","行駛日":"1234567"}, {"車次": "1504","行駛日":"12345"},{"車次": "1508","行駛日":"12345"},{"車次": "1510","行駛日":"1"},{"車次": "802","行駛日":"1234567"},{"車次": "1202","行駛日":"12345"},{"車次": "1602","行駛日":"12345"},{"車次": "1302","行駛日":"12345"},{"車次": "204","行駛日":"1234567"},{"車次": "606","行駛日":"1234567"},{"車次": "1514","行駛日":"12345"}]';
 
 function sendTextMessage(sender, text) {
-    var json='[ { "車次": "502", "時刻表": [ {  "地點": "台中","時間": "06:25"}, { "地點": "苗栗", "時間": "06:44"  } ] }]';
-    var location=new Array('台中','苗栗','新竹','桃園','板橋','台北','南港');
-    var data=JSON.parse(json);
-    var ans;
-    var time;
-    //  ans=text;
-     location.map(function(value){
-         if(text==value){
-            ans=text;
-         }
-     })
-     if(ans!=""){
-          ans="success"
-          for (let i=0;i<data[0].時刻表.length;i++){
-                if(text==data[0].時刻表[i].地點){
-                    ans=data[0].時刻表[i].時間;
-                    break
-                }
+    var inputText=text.split(",");
+    var start=inputText[0];
+    var end=inputText[1];
+    var startDataIndex,endDataIndex;
+
+    var d=new Date();
+    var stationParse=JSON.parse(station);
+    var trainGoParse=JSON.parse(trainGo);
+    var trainGoResult=new Array();
+    
+     for(let i=0;i<stationParse.length;i++){
+            if(start==stationParse[i]){
+                startDataIndex=i;
             }
-     }else{
-         ans="error"
-     }
+            if(end==stationParse[i]){
+                endDataIndex=i;
+            }
+    }
+
+    for(let i=0;i<trainGoParse.length;i++){
+        let temp=trainGoParse[i].行駛日;
+        if(temp.indexOf(d.getDay)>=0){
+            trainGoResult.push(temp);
+        }
+    }
+    var x=stationParse[startDataIndex].時刻表.length
+    var y=stationParse[endDataIndex].時刻表.length
+    var resultTime=start+"=>"+end;
+    while(x==-1||y==-1){
+        let i=trainGoResult.indexOf(stationParse[startDataIndex].時刻表[x].車次);
+        let j=trainGoResult.indexOf(stationParse[startDataIndex].時刻表[y].車次);
+        if(i>=0 && j>=0 ){
+            resultTime+="\n"+stationParse[startDataIndex].時刻表[x].車次+"  "+stationParse[startDataIndex].時刻表[x].時間+"=>"+stationParse[endDataIndex].時刻表[y].時間
+        }
+        x--;
+        y--;
+    }
+    // stationParse[startDataIndex].時刻表[j].車次
+    //     stationParse[endDataIndex].時刻表[j].車次
+  
+    // var json='[ { "車次": "502", "時刻表": [ {  "地點": "台中","時間": "06:25"}, { "地點": "苗栗", "時間": "06:44"  } ] }]';
+    // var location=new Array('台中','苗栗','新竹','桃園','板橋','台北','南港');
+    // var data=JSON.parse(json);
+    // var ans;
+    //  location.map(function(value){
+    //      if(text==value){
+    //         ans=text;
+    //      }
+    //  })
+    //  if(ans!=""){
+    //       ans="success"
+    //       for (let i=0;i<data[0].時刻表.length;i++){
+    //             if(text==data[0].時刻表[i].地點){
+    //                 ans=data[0].時刻表[i].時間;
+    //                 break
+    //             }
+    //         }
+    //  }else{
+    //      ans="error"
+    //  }
             
-    let messageData = { text:ans }
+    let messageData = { text:resultTime }
     request({
         url: 'https://graph.facebook.com/v2.7/me/messages',
         qs: {access_token:token},
@@ -220,3 +259,6 @@ function sendGenericMessage(sender) {
         }
     })
 }
+
+
+
